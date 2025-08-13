a4b0a9584f4b007eaa7bbbe3fa765b6e879416d3
Sagar Sahore <65637133+sagarsahore@users.noreply.github.com>
2025-08-13 05:28:25 +0000

    Day1: Project

---
+++ week 1/Day01_Basics.py
@@ -0,0 +1,12 @@
+#Working with Variables
+print("Hello World!\n yo" + " " +"Sagar")
+
+#input Funtion
+input("what is your name")
+print("Hello" + input("what is your name")+"!Sagar")
+
+#project 1
+print("Welcome to the band Name generator")
+a = input("Whats the name of the city you grew up in")sdfds
+b = input( "Whats your Pet name?")
+print( "Your Band name could be " + a + " " +b )
