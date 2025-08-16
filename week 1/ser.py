61ac53a16c29689da00ff2cdd6b3c0204f9bc262
Sagar Sahore <65637133+sagarsahore@users.noreply.github.com>
2025-08-16 11:20:06 +0000

    update;

--- week 1/ser.py
+++ week 1/ser.py
@@ -7,3 +7,11 @@ def factorial(n):
 
     #test the function
 print(factorial(5))  # Output: 120
+#write a function for calculating the nth Fibonacci number
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
