01e975c037874d6b9770f32ebbf4138ef2b21d7c
Sagar Sahore <65637133+sagarsahore@users.noreply.github.com>
2025-08-17 11:05:46 +0000

    tech: PE

--- week 1/ser.py
+++ week 1/ser.py
@@ -24,3 +24,23 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
+
+#Write a function to find the maximum element in a list
+def find_maximum(lst):
+    if not lst:
+        return None
+    max_element = lst[0]
+    for num in lst:
+        if num > max_element:
+            max_element = num
+    return max_element
+ #Write a function to find the minimum element in a list
+def find_minimum(lst):
+    if not lst:
+        return None
+    min_element = lst[0]
+    for num in lst:
+        if num < min_element:
+            min_element = num
+    return min_element
+
