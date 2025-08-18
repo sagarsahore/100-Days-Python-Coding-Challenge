8e7baf3a22302208d3e7d4ca1d45dab06864572c
Sagar Sahore <65637133+sagarsahore@users.noreply.github.com>
2025-08-18 21:54:11 +0000

    :Check PR

--- week 1/day2_DataTypes.py
+++ week 1/day2_DataTypes.py
@@ -26,4 +26,11 @@ tip_as_percent = tip / 100
 total_tip = bill * tip_as_percent
 total_bill = bill + total_tip
 bill_per_person = total_bill / people
-print(f"Each person should pay: ${round(bill_per_person, 2)}")10
+print(f"Each person should pay: ${round(bill_per_person, 2)}")
+
+#Calculate value of Log1 to log10
+print(math.log10(1))
+print(math.log10(10))
+print(math.log10(100))
+print(math.log10(1000))
+
